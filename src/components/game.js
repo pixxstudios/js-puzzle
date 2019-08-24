183167ded4286a23b6f258ef40a3ebcabc0c0406
Gagandeep Singh <gagan.ddun@gmail.com>
2019-08-24 23:02:42 +1000

    updated game class

--- src/components/game.js
+++ src/components/game.js
@@ -2,5 +2,20 @@ export class Game {
     constructor() {
         this.gameContainer = document.createElement('div');
         this.gameContainer.className = 'game-container';
+
+        const boardSize = 4;
+        const tileSize = 100;
+
+        this.board = new Board({
+            boardSize,
+            tileSize,
+            onGameWin: () => onGameWin()
+        });
+
+        this.gameContainer.appendChild(this.board.element);
+    }
+
+    get element() {
+        return this.gameContainer;
     }
 }  
