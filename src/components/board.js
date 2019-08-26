e1ee2518c3b3625dc24cbe9315f155ef6ca4c0a1
Gagandeep Singh <gagan.ddun@gmail.com>
2019-08-26 21:16:05 +1000

    update

--- src/components/board.js
+++ src/components/board.js
@@ -3,11 +3,34 @@ export class Board {
         this.boardSize = boardSize;
         this.tileSize = tileSize;
         this.onGameWin = onGameWin;
+        this.tileMargin = 0;
+        this.tiles = [];
+        this.tileOrder = [];
+
+        this.boardContainer = document.createElement('div');
+        this.boardContainer.className = 'board-container';
+
+        this.setTileMargin();
+        this.initBoard();
     }
 
     // create tile elements and add to the board
     initBoard() {
+        const numTiles = Math.pow(this.boardSize, 2);
+        this.tiles = [];
 
+        this.tileOrder = Array(numTiles).fill(0).map((el, i) => i);
+
+        /* do {
+            Helpers.shuffleArray(this.tileOrder);
+        } while(!this.isSolvable() || this.hasWon()) */
+
+        // add tiles to board
+        let curRow = 1;
+        let curCol = 1;
+        for(let i=0; i<numTiles; i++) {
+
+        }
     }
 
     ontileClick() {
