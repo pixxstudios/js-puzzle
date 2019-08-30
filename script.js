91af8730d904b8c75e216d915dfe3df2aa460330
Gagandeep Singh <gagan.ddun@gmail.com>
2019-08-30 22:42:34 +1000

    updated tiles

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 (function(){
+    // variables
     let Tiles = [];
+
+
+    // create the board
     let board = document.createElement('div');
     board.classList.add('board');
-
     document.querySelector('body').appendChild(board);
 
-    for(let i=0 ; i<15; i++) {
+    // Create the tiles
+    for(let i=1 ; i<16; i++) {
         let tile = document.createElement('div');
         tile.classList.add('tile');
+        tile.innerHTML = i.toString();
 
         Tiles.push(tile);
         board.appendChild(tile);
