7b60fcc8897d1f907fc8a313ddf62fb27271580f
Gagandeep Singh <gagan.ddun@gmail.com>
2019-09-02 18:45:46 +1000

    shuffle blank tile

--- script.js
+++ script.js
@@ -15,17 +15,14 @@ import { Shuffle } from './helpers.js';
     blackTile.classList.add('blankTile');
     Tiles.push(blackTile);
 
-    Shuffle(TileNums);
-    board.appendChild(blackTile);
-
     // Create the tiles
     for(let i=1 ; i<16; i++) {
         let tile = document.createElement('div');
         tile.classList.add('tile');
         tile.innerHTML = TileNums[i-1].toString();
-
         Tiles.push(tile);
-        board.appendChild(tile);
     }
-    console.log(Tiles);
+
+    Shuffle(Tiles);
+    Tiles.forEach(tile => board.appendChild(tile));
 })();
