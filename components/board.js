dfebad5fe1b4d4e6054418ebda00ce1fbc956cca
Gagandeep Singh <gagan.ddun@gmail.com>
2019-10-18 21:25:37 +1100

    tiles move logic

--- components/board.js
+++ components/board.js
@@ -1,40 +1,46 @@
 import Tiles from './tiles.js';
-import { Shuffle, TileClick } from '../helpers.js';
-
-export default class Board{
+import {
+    Shuffle,
+    TileClick
+} from '../helpers.js';
+export default class Board {
     constructor(boardSize) {
         this.boardSize = boardSize;
         this.numTiles = Math.pow(this.boardSize, 2);
         this.Tiles = [];
     }
-
     addTiles(i) {
         const tiles = new Tiles(i);
         this.Tiles.push(tiles.createTiles());
     }
-
     clearBoard() {
         const board = document.querySelector('.board');
         if (board !== null) {
             document.querySelector('body').removeChild(board);
         }
     }
-
     createBoard() {
         // create the board
         this.board = document.createElement('div');
         this.board.classList.add('board');
         document.querySelector('body').appendChild(this.board);
-
-        for(var i=0; i<this.numTiles; i++) {
+        for (var i = 0; i < this.numTiles; i++) {
             this.addTiles(i);
         }
-
         Shuffle(this.Tiles);
         this.Tiles.forEach(tile => {
             this.board.appendChild(tile);
-            tile.addEventListener('click', function() {
-                TileClick(this.innerHTML);
+            const _ = this;
+            const board = this.board;
+            tile.addEventListener('click', function () {
+                TileClick(this.innerHTML,
+                    function (newTiles) {
+
+                        _.innerHTML = "";
+                        newTiles.forEach(t => {
+                            board.appendChild(t);
+                        })
+                    });
             });
         })
     }
--- helpers.js
+++ helpers.js
@@ -1,5 +1,4 @@
 let tiles;
-
 export function Shuffle(array) {
     // randomly shuffle the array
     // return (arr.sort(() => Math.random() - 0.5));
@@ -11,35 +10,64 @@ export function Shuffle(array) {
     }
 }
 
-export function TileClick(position) {
+function canSwap(left, right, top, bottom) {
+    if (left === "" ||
+        right === "" ||
+        top === "" ||
+        bottom === "") {
+        return true;
+    }
+    return false;
+}
+export function TileClick(position, callback) {
     let index;
     let emptyTile;
     let left, right, top, bottom;
     tiles.forEach((tile, i) => {
         if (tile.innerHTML === position) {
+
             index = i;
         }
-
-        if(tile.innerHTML === '') {
+        if (tile.innerHTML === '') {
             emptyTile = i;
         }
-
         if (emptyTile === i) return;
     });
     // find neighbours
     // left
     // if position % 4 === 0 then no left neighbour
-    if (index === 0 || index%4 === 0) {
+    if (index === 0 || index % 4 === 0) {
         left = null;
+    } else {
+        left = tiles[index - 1].innerHTML;
     }
     // right
     // if position % 3 === 0 then no right neighbour
-    if (index === 0 || index%4 === 0) {
-        left = null;
+    if (index === 3 ||
+        index === 7 ||
+        index === 11 ||
+        index === 15) {
+        right = null;
     } else {
-        left = tiles[index-1];
+        right = tiles[index + 1].innerHTML;
     }
     // top
+    if (index < 4) {
+        top = null;
+    } else {
+        top = tiles[index - 4].innerHTML;
+    }
     // bottom
-    console.log(position, index, emptyTile, left);
+    if (index > 11) {
+        bottom = null;
+    } else {
+        bottom = tiles[index + 4].innerHTML;
+    }
+    if (canSwap(left, right, top, bottom)) {
+        let temp = tiles[index];
+
+        tiles[index] = tiles[emptyTile];
+        tiles[emptyTile] = temp;
+        callback(tiles);
+    }
 }
